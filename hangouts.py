88bcc80ebc1a2d966af7ab0f09332d903caeaf54
rmaratos <rmaratos@andrew.cmu.edu>
2014-07-26 01:20:30 -0400

    Further code cleanups

--- hangouts.py
+++ hangouts.py
@@ -4,6 +4,7 @@ import argparse
 import json
 
 from datetime import datetime
+from operator import attrgetter
 
 class Participant(object):
     def __init__(self, gaia_id, chat_id, name):
@@ -11,45 +12,21 @@ class Participant(object):
         self.gaia_id = gaia_id
         self.chat_id = chat_id
 
-    def get_id(self):
-        return self.gaia_id
-
-    def get_name(self):
-        return self.name
-
     def __unicode__(self):
-        if self.get_name() is None:
-            return self.get_id()
+        if self.name is None:
+            return self.gaia_id
         else:
-            return self.get_name()
+            return self.name
 
 class ParticipantList(object):
     def __init__(self):
         self.p_list = {}
-        self.current_iter = 0
-        self.max_iter = 0
 
     def add(self, p):
-        self.p_list[p.get_id()] = p
-        return self.p_list
+        self.p_list[p.gaia_id] = p
 
-    def get_by_id(self, id):
-        try:
-            return self.p_list[id]
-        except:
-            return None
-
-    def __iter__(self):
-        self.current_iter = 0
-        self.max_iter = len(self.p_list)-1
-        return self
-
-    def next(self):
-        if self.current_iter > self.max_iter:
-            raise StopIteration
-        else:
-            self.current_iter += 1
-            return self.p_list.values()[self.current_iter-1]
+    def get_by_id(self, gaia_id):
+        return self.p_list.get(gaia_id)
 
     def __unicode__(self):
         return ", ".join([unicode(p) for p in self.p_list.values()])
@@ -61,49 +38,9 @@ class Event(object):
         self.timestamp = timestamp
         self.message = message
 
-    def get_id(self):
-        return self.event_id
-
-    def get_sender_id(self):
-        return self.sender_id
-
-    def get_timestamp(self):
-        return self.timestamp
-
-    def get_message(self):
-        return self.message
-
     def get_formatted_message(self):
         # formatted message (the messages are joined by a space).
         return " ".join(self.message)
-
-class EventList(object):
-    def __init__(self):
-        self.event_list = {}
-        self.current_iter = 0
-        self.max_iter = 0
-
-    def add(self, e):
-        self.event_list[e.get_id()] = e
-        return self.event_list
-
-    def get_by_id(self, id):
-        try:
-            return self.event_list[id]
-        except:
-            return None
-
-    def __iter__(self):
-        self.current_iter = 0
-        self.max_iter = len(self.event_list)-1
-        return self
-
-    def next(self):
-        if self.current_iter > self.max_iter:
-            raise StopIteration
-        else:
-            self.current_iter += 1
-            return self.event_list.values()[self.current_iter-1]
 
 class Conversation(object):
     def __init__(self, conversation_id, participants, events):
@@ -111,17 +48,8 @@ class Conversation(object):
         self.participants = participants
         self.events = events
 
-    def get_id(self):
-        return self.conversation_id
-
-    def get_participants(self):
-        return self.participants
-
     def get_events(self):
-        return sorted(self.events, key=lambda event: event.get_timestamp())
-
-    def get_events_unsorted(self):
-        return self.events
+        return sorted(self.events, key=attrgetter("timestamp"))
 
 class HangoutsReader(object):
     def __init__(self, logfile, conversation_id=None):
@@ -138,16 +66,16 @@ class HangoutsReader(object):
         for conversation in data["conversation_state"]:
             c = self.extract_conversation_data(conversation)
             if self.conversation_id is None:
-                self.print_("conversation id: %s, participants: %s" % (c.get_id(), unicode(c.get_participants())))
-            elif c.get_id() == self.conversation_id:
+                self.print_("conversation id: {}, participants: {}".format(c.conversation_id, unicode(c.participants)))
+            elif c.conversation_id == self.conversation_id:
                 self.print_conversation(c)
 
     def print_conversation(self, conversation):
-        participants = conversation.get_participants()
+        participants = conversation.participants
         for event in conversation.get_events():
             self.print_(u"[{timestamp}] {author}: {message}".format(
-                        timestamp = datetime.fromtimestamp(long(long(event.get_timestamp())/10**6.)),
-                        author = participants.get_by_id(event.get_sender_id()).get_name(),
+                        timestamp = datetime.fromtimestamp(long(long(event.timestamp)/10**6.)),
+                        author = participants.get_by_id(event.sender_id).name,
                         message = event.get_formatted_message(),
                     ))
 
@@ -161,14 +89,11 @@ class HangoutsReader(object):
             for participant in conversation["conversation_state"]["conversation"]["participant_data"]:
                 gaia_id = participant["id"]["gaia_id"]
                 chat_id = participant["id"]["chat_id"]
-                try:
-                    name = participant["fallback_name"]
-                except KeyError:
-                    name = None
+                name = participant.get("fallback_name")
                 p = Participant(gaia_id, chat_id, name)
                 participant_list.add(p)
 
-            event_list = EventList()
+            event_list = list()
 
             for event in conversation["conversation_state"]["event"]:
                 event_id = event["event_id"]
@@ -190,7 +115,7 @@ class HangoutsReader(object):
                 except KeyError:
                     continue # that's okay
                 # finally add the event to the event list
-                event_list.add(Event(event_id, sender_id["gaia_id"], timestamp, text))
+                event_list.append(Event(event_id, sender_id["gaia_id"], timestamp, text))
         except KeyError:
             raise RuntimeError("The conversation data could not be extracted.")
         return Conversation(conversation_id, participant_list, event_list)
@@ -212,7 +137,6 @@ def main(argv):
     args = parser.parse_args()
 
     hr = HangoutsReader(args.logfile, conversation_id=args.conversation_id)
-
 
 if __name__ == "__main__":
     main(sys.argv)
